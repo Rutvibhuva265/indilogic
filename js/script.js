c892f1309519fcf86f5352cee0c54442df7ae75f
Rutvibhuva265 <166202715+Rutvibhuva265@users.noreply.github.com>
2024-09-10 10:30:55 +0530

    add new

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const mainMenu = document.querySelector('.main-menu');
+
+  menuToggle.addEventListener('click', function() {
+    // Toggle the 'active' class to show/hide the menu
+    mainMenu.classList.toggle('active');
+    
+    // Change icon to close (X) when menu is open, revert to hamburger when closed
+    if (mainMenu.classList.contains('active')) {
+      menuToggle.innerHTML = `
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M14.7886 1L0.999992 14.7886" stroke="black"/>
+        <path d="M14.7886 15L0.999992 1.21142" stroke="black"/>
+        </svg>`;
+    } else {
+      menuToggle.innerHTML = `<svg width="23" height="13" viewBox="0 0 23 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0.5 1H22.5M0.5 12H22.5M0.5 6.5H22.5" stroke="black"/>
+        </svg>`;
+    }
+  });
+});
